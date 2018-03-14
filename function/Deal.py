463ba188cfd4b010329c3702237365b18d43054c
Liusii <13001211864@163.com>
2018-03-14 23:21:15 +0800

    first commit

--- function/Deal.py
+++ function/Deal.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 
 def split(image):
@@ -7,13 +8,43 @@ def split(image):
     return ch3, ch2, ch1
 
 
-def translate(image):
-    # 去除阴影，所有像素遍历
-    h, w, c= image.shape
+def k_means(image, k):
+    # k_means实现，输入k值
+
+    # k_means读入
+    img_shape = image.reshape((-1, 3))
+    # 转换为float类型
+    data = np.float32(img_shape)
+    # 定义标准、K值
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    # 调用k_means函数
+    ret, label, center = cv.kmeans(data, k, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
+    # 转回uint8格式，恢复图像
+    center = np.uint8(center)
+    k_img_ori = center[label.flatten()]
+    k_img = k_img_ori.reshape((image.shape))
+
+    return k_img
+
+
+def translate(image, src_image):
+    # 去除阴影，所有像素遍历，近黑色转为黑色，其他转为白色，与源图像srcIamge作加处理，实现将阴影变为白色
+
+    # 遍历将图转为二值
+    h, w, c = image.shape
     for row in range(h):
         for col in range(w):
              if (image[row, col, 0] < 60) & (image[row, col, 1] < 60) & (image[row, col, 2] < 60):
                 image[row, col, 0] = 255
                 image[row, col, 1] = 255
                 image[row, col, 2] = 255
-    return image
+             else:
+                image[row, col, 0] = 0
+                image[row, col, 1] = 0
+                image[row, col, 2] = 0
+
+    # 与源图像进行加处理
+    f_image = cv.add(image, src_image)
+
+    return f_image
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from function import Deal
 img = IO.img_in("resources/map2.jpg")
 IO.img_out("original", img)
 
+"""
 # RGB分离
 B, G, R = Deal.split(img)
 # RGB转HSV并输出
@@ -21,31 +22,18 @@ hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
 # IO.img_out("hsv", hsv)
 # HSV分离并输出
 H, S, V = Deal.split(hsv)
-"""
+
 IO.img_out("H", H)
 IO.img_out("S", S)
 IO.img_out("V", V)
 """
-# k_means读入
-img_shape = img.reshape((-1, 3))
-# 转换为float类型
-data = np.float32(img_shape)
-# 定义标准、K值
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 6
-# 调用k_means函数
-ret, label, center = cv.kmeans(data, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+# k_means实现
+k_img = Deal.k_means(img, 6)
 
-# 转回uint8格式，恢复图像
-center = np.uint8(center)
-k_img_ori = center[label.flatten()]
-k_img = k_img_ori.reshape((img.shape))
-
-# 消除阴影为白色
-dst = Deal.translate(k_img)
+# 消除阴影为白色，其他部分还原
+dst = Deal.translate(k_img, img)
 
 cv.imshow("k-cluster", dst)
-
 
 # 等待
 cv.waitKey(0)
