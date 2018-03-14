0c9fc06032a85f6eba0d1753a51dec5b422f17de
Liusii <13001211864@163.com>
2018-03-14 22:55:14 +0800

    first commit

--- function/Deal.py
+++ function/Deal.py
@@ -1,6 +1,19 @@
 import cv2 as cv
 
+
 def split(image):
     # 分量的提取
     ch1, ch2, ch3 = cv.split(image)
     return ch3, ch2, ch1
+
+
+def translate(image):
+    # 去除阴影，所有像素遍历
+    h, w, c= image.shape
+    for row in range(h):
+        for col in range(w):
+             if (image[row, col, 0] < 60) & (image[row, col, 1] < 60) & (image[row, col, 2] < 60):
+                image[row, col, 0] = 255
+                image[row, col, 1] = 255
+                image[row, col, 2] = 255
+    return image
--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 """
 import cv2 as cv
 import numpy as np
+
+# 导入其他文件函数库
 from function import IO
 from function import Deal
 
@@ -12,46 +14,37 @@ from function import Deal
 img = IO.img_in("resources/map2.jpg")
 IO.img_out("original", img)
 
-# RGB输出
+# RGB分离
 B, G, R = Deal.split(img)
-"""
-img_out("R", R)
-img_out("G", G)
-img_out("B", B)
-"""
-
-# RGB转HSV
+# RGB转HSV并输出
 hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
 # IO.img_out("hsv", hsv)
-
-# HSV输出
+# HSV分离并输出
 H, S, V = Deal.split(hsv)
 """
-img_out("H", H)
-img_out("S", S)
-img_out("V", V)
+IO.img_out("H", H)
+IO.img_out("S", S)
+IO.img_out("V", V)
 """
-
-# kmeans读入
-Z = img.reshape((-1, 3))
-
+# k_means读入
+img_shape = img.reshape((-1, 3))
 # 转换为float类型
-Z = np.float32(Z)
-
+data = np.float32(img_shape)
 # 定义标准、K值
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 5
-
+K = 6
 # 调用k_means函数
-ret, label, center = cv.kmeans(Z, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+ret, label, center = cv.kmeans(data, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
 
-# Now convert back into uint8, and make original image
 # 转回uint8格式，恢复图像
 center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape((img.shape))
+k_img_ori = center[label.flatten()]
+k_img = k_img_ori.reshape((img.shape))
 
-cv.imshow("k-cluster", res2)
+# 消除阴影为白色
+dst = Deal.translate(k_img)
+
+cv.imshow("k-cluster", dst)
 
 
 # 等待
